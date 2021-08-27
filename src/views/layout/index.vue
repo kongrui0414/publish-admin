9b9f9a5683c8bdd14c6078b846e2add6819b30c8
currykong <currykong@tenvine.cn>
2021-08-27 18:16:50 +0800

    feat: header初始化

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -6,7 +6,26 @@
       <AppAside class="aside-menu"/>
     </el-aside>
     <el-container>
-      <el-header class="header">Header</el-header>
+      <el-header class="header">
+        <div>
+          <i class="el-icon-s-fold"></i>
+          库里后台管理系统
+        </div>
+        <el-dropdown>
+          <div class="avatar-wrap">
+            <img
+              class="avatar"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHUfiEdw5mFHEIW7XLjdzj98lTiZCu0NXUsw&usqp=CAU"
+              alt="">
+            <span>用户昵称</span>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </div>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>设置</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
       <el-main class="main">
         <!--  子路由出口-->
         <router-view/>
@@ -24,6 +43,7 @@
 
 <script>
 import AppAside from './components/aside'
+
 export default {
   name: 'LayoutIndex',
   components: {
@@ -47,13 +67,28 @@ export default {
 }
 
 .header {
-  background-color: #B3C0D1;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
 }
 
 .main {
   background-color: #E9EEF3;
 }
+
 .aside-menu {
   height: 100%;
+}
+.avatar-wrap {
+  display: flex;
+  align-items: center;
+}
+.avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-right: 10px;
 }
 </style>
