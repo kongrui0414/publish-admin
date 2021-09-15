05d10b38be669916e5282da2ce50f2071a9e9d1b
currykong <currykong@tenvine.cn>
2021-09-15 10:44:21 +0800

    feat: 发布文章 请求提交

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -34,8 +34,8 @@
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">发表</el-button>
-        <el-button>存入草稿</el-button>
+        <el-button type="primary" @click="onPublish(false)">发表</el-button>
+        <el-button @click="onPublish(true)">存入草稿</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -43,7 +43,7 @@
 </template>
 
 <script>
-import { getArticleChannels } from '@/api/article'
+import { getArticleChannels, addArticle } from '@/api/article'
 
 export default {
   name: 'PublishIndex',
@@ -73,7 +73,17 @@ export default {
         this.channels = data.channels
       })
     },
-    onSubmit () {
+    onPublish (draft = false) {
+      // 找到数据接口
+      // 封装请求方法
+      // 请求提交表单
+      addArticle(this.article, draft).then(res => {
+        this.$message({
+          message: '发布成功',
+          type: 'success'
+        })
+      })
+      // 处理响应结果
       console.log('submit!')
     }
   }
