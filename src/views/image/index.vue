40466699ea83e9af753ed5a64d7ef6e60be603eb
currykong <currykong@tenvine.cn>
2021-09-16 17:39:29 +0800

    feat: 上传素材按钮弹框用dialog，使用遮罩层

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -1,44 +1,59 @@
 <template>
-<div class="image-container">
-  <el-card class="box-card">
-    <div slot="header" class="clearfix">
-      <!--    面包屑导航-->
-      <el-breadcrumb separator-class="el-icon-arrow-right">
-        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item>素材管理</el-breadcrumb-item>
-      </el-breadcrumb>
-<!--      <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>-->
-    </div>
-    <div style="padding-bottom: 20px">
-      <el-radio-group
-        v-model="collect"
-        size="medium"
-        @change="onCollectChange"
-      >
-        <el-radio-button
-          :label="false"
-        >全部</el-radio-button>
-        <el-radio-button
-          :label="true"
-        >收藏</el-radio-button>
-      </el-radio-group>
-    </div>
-    <el-row :gutter="10">
-      <el-col
-        :lg="4"
-        :md="6"
-        :xs="12"
-        v-for="(img, index) in images"
-        :key="index"
-      >
-        <el-image
-          style="min-width: 200px; height: 150px"
-          :src="img.url"
-          :fit="cover"></el-image>
-      </el-col>
-    </el-row>
-  </el-card>
-</div>
+  <div class="image-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!--    面包屑导航-->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>素材管理</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!--      <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>-->
+      </div>
+      <div class="action-head">
+        <el-radio-group
+          v-model="collect"
+          size="medium"
+          @change="onCollectChange"
+        >
+          <el-radio-button
+            :label="false"
+          >全部
+          </el-radio-button>
+          <el-radio-button
+            :label="true"
+          >收藏
+          </el-radio-button>
+        </el-radio-group>
+        <el-button
+          size="medium"
+          type="success"
+          @click="dialogUploadVisible = true"
+        >上传素材
+        </el-button>
+      </div>
+      <el-row :gutter="10">
+        <el-col
+          :lg="4"
+          :md="6"
+          :xs="12"
+          v-for="(img, index) in images"
+          :key="index"
+        >
+          <el-image
+            style="min-width: 200px; height: 150px"
+            :src="img.url"
+            :fit="cover"></el-image>
+        </el-col>
+      </el-row>
+    </el-card>
+    <el-dialog
+      title="上传素材"
+      :visible.sync="dialogUploadVisible"
+      :append-to-body="true"
+    >
+      hello curry
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -49,7 +64,8 @@ export default {
   data () {
     return {
       collect: false,
-      images: []
+      images: [],
+      dialogUploadVisible: false
     }
   },
   created () {
@@ -70,6 +86,10 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.action-head {
+  padding-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
